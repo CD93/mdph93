07799071a614a6e7f4c5eb020d06b860c8fcaabd
clydeb <33129994+bclydeb@users.noreply.github.com>
2018-01-05 15:59:12 +0100

    on ajoute des asterisques partout pour la recherche fulltexte

--- spip/config/mes_options.php
+++ spip/config/mes_options.php
@@ -6,5 +6,5 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 }
 
 define('_SURLIGNE_RECHERCHE_REFERERS',true);
-
+defined('_FULLTEXT_ASTERISQUE_PARTOUT') || define('_FULLTEXT_ASTERISQUE_PARTOUT', true);
 ?>
