2c728e05f6bad80feeb9ae55a386af2296cbeb55
clydeb <33129994+bclydeb@users.noreply.github.com>
2018-01-09 14:17:29 +0100

    formulaire de demande d'avis utilisateur

---
+++ spip/squelettes/formulaires/avis_utilisateur.php
@@ -0,0 +1,29 @@
+<?php
+
+/***************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2016                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+function formulaires_avis_utilisateur_charger_dist() {
+	$valeurs = array();
+	return $valeurs;
+}
+
+function formulaires_avis_utilisateur_verifier_dist() {
+	$erreurs = array();
+	return $erreurs;
+}
+
+function formulaires_avis_utilisateur_traiter_dist() {
+}
