bb029cce7c5d48e297c31e39fb37f45a4c67af45
clydeb <33129994+bclydeb@users.noreply.github.com>
2018-01-09 11:56:07 +0100

    footer en inclure et ajout de piwik

--- spip/config/mes_options.php
+++ spip/config/mes_options.php
@@ -6,5 +6,8 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 }
 
 define('_SURLIGNE_RECHERCHE_REFERERS',true);
+if (isset($_REQUEST['recherche'])) {
+  $_GET['var_recherche'] = $_REQUEST['recherche'];
+}
 defined('_FULLTEXT_ASTERISQUE_PARTOUT') || define('_FULLTEXT_ASTERISQUE_PARTOUT', true);
 ?>
