4325b955b98022be0541b5259a7e810610428c6e
Samuel Godo <sgodo@seinesaintdenis.fr>
2018-03-06 09:41:03 +0100

    les formulaires

--- spip/squelettes/formulaires/ecrire.php
+++ spip/squelettes/formulaires/ecrire.php
@@ -19,23 +19,21 @@ function formulaires_ecrire_charger_dist() {
 	include_spip('inc/texte');
 	$puce = definir_puce();
 	$valeurs = array(
+		'nombre_a' => rand(1, 10),
+		'nombre_b' => rand(1, 10),
 		'sujet_message_auteur' => '',
 		'texte_message_auteur' => '',
 		'email_message_auteur' => isset($GLOBALS['visiteur_session']['email']) ?
 			$GLOBALS['visiteur_session']['email'] : '',
 		'nobot' => '',
+		'test' => ''
 	);
-
-	// id du formulaire (pour en avoir plusieurs sur une meme page)
-
-
 	return $valeurs;
 }
 
 function formulaires_ecrire_verifier_dist() {
 	$erreurs = array();
 	include_spip('inc/filtres');
-
 	if (!$adres = _request('email_message_auteur')) {
 		$erreurs['email_message_auteur'] = _T('info_obligatoire');
 	} elseif (!email_valide($adres)) {
@@ -60,17 +58,16 @@ function formulaires_ecrire_verifier_dist() {
 	if (_request('nobot')) {
 		$erreurs['message_erreur'] = _T('pass_rien_a_faire_ici');
 	}
-
-	if (!_request('confirmer') and !count($erreurs)) {
-		$erreurs['previsu'] = ' ';
-		$erreurs['message_erreur'] = '';
+	if(_request('test') != _request('nba')+_request('nbb')){
+		$erreurs['message_erreur'] = "mauvaise réponse au calcul";
 	}
 
 	return $erreurs;
 }
 
 function formulaires_ecrire_traiter_dist() {
-	$mail="sgodo@seinesaindenis.fr";
+
+	$mail="contact@seinesaindenis.fr";
 	$adres = _request('email_message_auteur');
 	$sujet = _request('sujet_message_auteur');
 
@@ -78,20 +75,30 @@ function formulaires_ecrire_traiter_dist() {
 		. _T('info_message_2') . ' '
 		. $sujet;
 	$texte = _request('texte_message_auteur');
-
 	$texte .= "\n\n-- " . _T('envoi_via_le_site') . ' '
 		. supprimer_tags(extraire_multi($GLOBALS['meta']['nom_site']))
 		. ' (' . $GLOBALS['meta']['adresse_site'] . "/) --\n";
+	$textear = "Nous avons bien reçu votre courriel, nos équipes vont transmettre votre demande au service concerné dans les deux jours ouvrés. Merci de nous avoir contacté. L'équipe web.<br/> votre message : <br/>".$texte;
 	$envoyer_mail = charger_fonction('envoyer_mail', 'inc');
-
-	if ($envoyer_mail($mail, $sujet, $texte, $adres,
-		'X-Originating-IP: ' . $GLOBALS['ip'])) {
-		$message = _T('form_prop_message_envoye');
-
-		return array('message_ok' => $message);
+	$corps = array(
+		'html' => $texte,
+		'texte' => $texte,
+		'repondre_a' => $adres
+	);
+	$corpsar = array(
+		'html' => $textear,
+		'texte' => $textear
+	);
+	$ar = ($envoyer_mail($adres, "accusé de reception ".$sujet, $corpsar, $mail,
+	  'X-Originating-IP: ' . $GLOBALS['ip']));
+	if ($envoyer_mail($mail, $sujet, $corps, $adres,
+	  'X-Originating-IP: ' . $GLOBALS['ip'])) {
+	  $message = "votre message a bien été envoyé, vous allez recevoir un accusé de reception par courriel";
+	  return array('message_ok' => $message);
 	} else {
-		$message = _T('pass_erreur_probleme_technique');
-
-		return array('message_erreur' => $message);
+	  $message = _T('pass_erreur_probleme_technique');
+	  return array('message_erreur' => $message);
 	}
+	return array('message_ok' => $message);
+
 }
